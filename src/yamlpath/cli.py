592567735ddde7af025d9375bb004214b10ae535
hejack0207 <hejack0207@sina.com>
2018-12-18 21:33:35 +0800

    fix

--- src/yamlpath/cli.py
+++ src/yamlpath/cli.py
@@ -1,11 +1,26 @@
 import click
 
+def append_path(data, keys,path):
+    if type(data) != dict:
+	return
+    for key in data:
+	keys.append('.'.join(path + [key]))
+	append_path(data[key], keys, path + [key])
+
 @click.command()
-@click.option('--name', prompt='Your name', help='The person to greet.')
-def cli(file):
+@click.argument('yamlfile', type=click.Path())
+def cli(yamlfile):
     """show all keys recursively for specified yaml file"""
-    for x in range(count):
-        click.echo('%s' % name)
+    keys=[]
+    try:
+        data = yaml.load(open(yamlfile))
+        append_path(data,keys, [])
+    except yaml.YAMLError:
+        click.echo(f"Warn: ParseError in {yamlfile}", err=True)
+
+    click.echo(keys)
 
 if __name__ == '__main__':
     cli()
+
+# vim: sts=-1 sw=4
